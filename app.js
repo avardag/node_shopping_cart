56f29648ce99402bd8772bccaacf34ee99229f9c
Dagestan05 <steklofan@gmail.com>
2018-08-24 17:07:36 +0200

    a. handlebars view-engine, layout wrapper, header & bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -10,11 +12,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine(".hbs", expressHbs({defaultLayout: 'layout', extname: ".hbs"}))
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
