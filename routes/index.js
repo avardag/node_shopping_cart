1a7ed2f1533ecdabb7850c61667ccd6e168fc2b8
Dagestan05 <steklofan@gmail.com>
2018-08-25 03:26:54 +0200

    rendered products fetched from DB to index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
 
+let Product = require("../models/product");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+router.get("/", function(req, res, next) {
+  //fetch products from DB
+  Product.find({}, (err, docs)=>{
+    if (err) {
+      console.log('err', err);
+    }
+    //serve three items at a time
+    let productChunks = [];
+    let chunkSize = 3;
+    for (let i = 0; i < docs.length; i += chunkSize) {
+      //will push array of three items into productChunks arr
+      productChunks.push(docs.slice(i, i+chunkSize))
+    }
+    res.render("shop/index", { title: "Shopping Cart", products: productChunks });
+  });
 });
 
 module.exports = router;
